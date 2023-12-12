df9d034c9f73152bf23278d975bc894ea5e8ca56
Mozahedul <mozahed001@gmail.com>
2023-12-12 22:38:23 +0600

    fix: :bug: href="#id" not working on Netlify - resolved
    <Link href="#id" works fine in localhost but not working on Netlify. Resolved the issue

--- app/components/navigation/page.jsx
+++ app/components/navigation/page.jsx
@@ -6,7 +6,7 @@ const Navigation = ({ handleMenuClose }) => {
     <div>
       <ul className="flex flex-col text-lg font-medium tracking-wide text-gray-300 md:flex-row md:text-sm md:font-semibold">
         <li className="m-5 md:m-3 lg:m-5">
-          <Link onClick={handleMenuClose} href="#about" className="navBtn">
+          <Link onClick={handleMenuClose} href="/#about" className="navBtn">
             <span className="text-cyan-300">01. </span>About
           </Link>
         </li>
@@ -17,13 +17,13 @@ const Navigation = ({ handleMenuClose }) => {
           </Link>
         </li>
         <li className="m-5 md:m-3 lg:m-5">
-          <Link onClick={handleMenuClose} href="#work" className="navBtn">
+          <Link onClick={handleMenuClose} href="/#work" className="navBtn">
             {" "}
             <span className="text-cyan-300">03. </span> Work
           </Link>
         </li>
         <li className="m-5 md:m-3 lg:m-5">
-          <Link onClick={handleMenuClose} href="#contact" className="navBtn">
+          <Link onClick={handleMenuClose} href="/#contact" className="navBtn">
             {" "}
             <span className="text-cyan-300">04. </span> Contact
           </Link>
